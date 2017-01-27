9996f2c959a3d814fd98be634153657759f4bd60
Matthew Iannucci <mpiannucci@gmail.com>
2017-01-27 13:28:59 -0500

    Working settings with correct schemas!

--- prefs.js
+++ prefs.js
@@ -3,6 +3,17 @@ const Gtk = imports.gi.Gtk;
 const GObject = imports.gi.GObject;
 const Lang = imports.lang;
 
+const Gettext = imports.gettext.domain('extendedgestures');
+const _ = Gettext.gettext;
+const Extension = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Extension.imports.convenience;
+
+let schema = null;
+
+function init() {
+    schema = Convenience.getSettings();
+}
+
 const ExtendedGesturesSettingsWidget = new GObject.Class({
     Name: 'ExtendedGestures.prefs.ExtendedGesturesSettingsWidget',
     GTypeName: 'ExtendedGesturesSettingsWidget',
@@ -21,7 +32,8 @@ const ExtendedGesturesSettingsWidget = new GObject.Class({
         this._horizontalSwitchLabel = new Gtk.Label ({label: "Horizontal Swipes"});
         this.add(this._horizontalSwitchLabel);
 
-        this._horizontalSwitch = new Gtk.Switch ({active: true});
+        this._horizontalSwitch = new Gtk.Switch ({active: schema.get_boolean ('horizontal-swipes')});
+        this._horizontalSwitch.connect('notify::active', Lang.bind (this, this._horizontalSwitchFlip));
         this.attach_next_to(this._horizontalSwitch, this._horizontalSwitchLabel, Gtk.PositionType.RIGHT, 1, 1);
 
         this._horizontalLabel = new Gtk.Label ({label: "Action"});
@@ -32,14 +44,19 @@ const ExtendedGesturesSettingsWidget = new GObject.Class({
         let actions = ["Toggle Overview", "Cycle Applications"];
         for (let i = 0; i < actions.length; i++)
             this._horizontalActionCombo.append_text (actions[i]);
-        this._horizontalActionCombo.set_active (0);
+        this._horizontalActionCombo.set_active (schema.get_enum('horizontal-action'));
+        this._horizontalActionCombo.connect ('changed', Lang.bind (this, this._horizontalActionChanged));
         this.attach_next_to(this._horizontalActionCombo, this._horizontalLabel, Gtk.PositionType.RIGHT, 1, 1);
+    },
+
+    _horizontalSwitchFlip: function() {
+        schema.set_boolean ('horizontal-swipes', this._horizontalSwitch.get_active());
+    }, 
+
+    _horizontalActionChanged: function() {
+        schema.set_enum ('horizontal-action', this._horizontalActionCombo.get_active());
     }
 });
-
-function init() {
-
-}
 
 function buildPrefsWidget() {
     let settingsWidget = new ExtendedGesturesSettingsWidget();
