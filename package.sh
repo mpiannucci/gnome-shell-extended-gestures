c4de757b345e6b77cadc1472608ff3abd8b151cb
Matthew Iannucci <mpiannucci@gmail.com>
2017-07-06 18:34:16 +0000

    Fix package script to have correct layout and add settings screenshot

--- package.sh
+++ package.sh
@@ -4,4 +4,5 @@ if [ -f ./extendedgestures@mpiannucci.github.com.zip ]; then
     rm -rf ./extendedgestures@mpiannucci.github.com.zip
 fi
 
-zip -r extendedgestures@mpiannucci.github.com.zip extendedgestures@mpiannucci.github.com
+cd extendedgestures@mpiannucci.github.com/
+zip -r ../extendedgestures@mpiannucci.github.com.zip *
