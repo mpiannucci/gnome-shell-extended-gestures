90c0d6ad8c2a4cc5cc9144ca078eada3d678d834
Matthew Iannucci <mpiannucci@gmail.com>
2017-11-14 16:21:55 -0500

    Add installation script

---
+++ install.sh
@@ -0,0 +1,5 @@
+if [ ! -d ~/.local/share/gnome-shell/extensions ]; then
+    mkdir ~/.local/share/gnome-shell/extensions
+fi
+
+cp -r extendedgestures@mpiannucci.github.com ~/.local/share/gnome-shell/extensions
