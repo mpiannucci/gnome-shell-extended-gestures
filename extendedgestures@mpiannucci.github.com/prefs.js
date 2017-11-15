9fce4f49aaec60f09aa76072804d26ba37d2c34c
Matthew Iannucci <mpiannucci@gmail.com>
2017-11-15 16:48:31 -0500

    Remove glade ui files and move settings to code only

--- extendedgestures@mpiannucci.github.com/prefs.js
+++ extendedgestures@mpiannucci.github.com/prefs.js
@@ -22,35 +22,71 @@ const ExtendedGesturesSettingsWidget = new GObject.Class({
     _init: function (params) {
         this.parent (params);
 
-        let builder = new Gtk.Builder ();
-        builder.add_from_file (Extension.path + '/prefs.glade');
-        builder.connect_signals(this);
+        this._buildUI();
+        this._initUI();
+    },
 
-        // The main grid
-        this._mainGrid = builder.get_object('mainGrid');
-        this.add(this._mainGrid);
+    _buildUI: function() {
+        // The swipe options grid setup
+        this._swipeOptionsFrame = new Gtk.Frame();
+        this._swipeOptionsFrame.set_label("Swipe Options");
+        this._swipeOptionsGrid = new Gtk.Grid();
+        this._swipeOptionsFrame.add(this._swipeOptionsGrid);
+        
+        // The swipe options
+        // Three finger horizontal
+        this._horizontalThreeLabel = new Gtk.Label({label: "3 Finger Horizontal Gestures"});
+        this._horizontalThreeSwitch = new Gtk.Switch();
+        this._horizontalThreeCombo = new Gtk.ComboBoxText();
+        this._swipeOptionsGrid.attach(this._horizontalThreeLabel, 0, 0, 1, 1);
+        this._swipeOptionsGrid.attach(this._horizontalThreeSwitch, 1, 0, 1, 1);
+        this._swipeOptionsGrid.attach(this._horizontalThreeCombo, 2, 0, 1, 1);
 
-        // Switches
-        this._horizontalThreeSwitch = builder.get_object('horizontalThreeSwitch');
-        schema.bind('horizontal-three-swipes', this._horizontalThreeSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
-        this._verticalThreeSwitch = builder.get_object('verticalThreeSwitch');
-        schema.bind('vertical-three-swipes', this._verticalThreeSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
-        this._horizontalFourSwitch = builder.get_object('horizontalFourSwitch');
+        // Three finger vertical
+        this._verticalThreeLabel = new Gtk.Label({label: "3 Finger Vertical Gestures"});
+        this._verticalThreeSwitch = new Gtk.Switch();
+        this._verticalThreeCombo = new Gtk.ComboBoxText();
+        this._swipeOptionsGrid.attach(this._verticalThreeLabel, 0, 1, 1, 1);
+        this._swipeOptionsGrid.attach(this._verticalThreeSwitch, 1, 1, 1, 1);
+        this._swipeOptionsGrid.attach(this._verticalThreeCombo, 2, 1, 1, 1);
+
+        // Four finger horizontal
+        this._horizontalFourLabel = new Gtk.Label({label: "4 Finger Horizontal Gestures"});
+        this._horizontalFourSwitch = new Gtk.Switch();
+        this._horizontalFourCombo = new Gtk.ComboBoxText();
+        this._swipeOptionsGrid.attach(this._horizontalFourLabel, 0, 2, 1, 1);
+        this._swipeOptionsGrid.attach(this._horizontalFourSwitch, 1, 2, 1, 1);
+        this._swipeOptionsGrid.attach(this._horizontalFourCombo, 2, 2, 1, 1);
+
+        // Four finger vertical
+        this._verticalFourLabel = new Gtk.Label({label: "4 Finger Vertical Gestures"});
+        this._verticalFourSwitch = new Gtk.Switch();
+        this._verticalFourCombo = new Gtk.ComboBoxText();
+        this._swipeOptionsGrid.attach(this._verticalFourLabel, 0, 3, 1, 1);
+        this._swipeOptionsGrid.attach(this._verticalFourSwitch, 1, 3, 1, 1);
+        this._swipeOptionsGrid.attach(this._verticalFourCombo, 2, 3, 1, 1);
+
+        // Add everything to the main view
+        this.add(this._swipeOptionsFrame);
+    },
+
+    _initUI: function() {
+        let actions = ['Toggle Overview', 'Cycle Applications', 'Show App Drawer', 'Switch Workspace'];
+
+        // Disable four finger options for now :(
         this._horizontalFourSwitch.set_active(false);
         this._horizontalFourSwitch.set_sensitive(false);
-        this._verticalFourSwitch = builder.get_object('verticalFourSwitch');
         this._verticalFourSwitch.set_active(true);
         this._verticalFourSwitch.set_sensitive(false);
 
-        // Combos
-        let actions = ['Toggle Overview', 'Cycle Applications', 'Show App Drawer', 'Switch Workspace'];
-        this._horizontalThreeCombo = builder.get_object('horizontalThreeActionCombo');
+        // Bind the three swipe toggles to their setting values
+        schema.bind('horizontal-three-swipes', this._horizontalThreeSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
+        schema.bind('vertical-three-swipes', this._verticalThreeSwitch, 'active', Gio.SettingsBindFlags.DEFAULT);
+
+        // Action set up
         this._horizontalThreeCombo.connect('changed', Lang.bind(this, this._horizontalThreeComboChanged));
-        this._verticalThreeCombo = builder.get_object('verticalThreeActionCombo');
         this._verticalThreeCombo.connect('changed', Lang.bind(this, this._verticalThreeComboChanged));
-        this._horizontalFourCombo = builder.get_object('horizontalFourActionCombo');
         this._horizontalFourCombo.set_sensitive(false);
-        this._verticalFourCombo = builder.get_object('verticalFourActionCombo');
         this._verticalFourCombo.set_sensitive(false);
         for (let i = 0; i < actions.length; i++) {
             this._horizontalThreeCombo.append_text(actions[i]);
